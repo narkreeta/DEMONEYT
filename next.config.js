54508847530019aca84ee67e03ce906b62d626f2
narkreeta <sailendradash@protonmail.com>
2022-11-14 21:30:27 +0530

    second

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: "build",
   // compiler: {
   //   // Enables the styled-components SWC transform
   //   styledComponents: true
